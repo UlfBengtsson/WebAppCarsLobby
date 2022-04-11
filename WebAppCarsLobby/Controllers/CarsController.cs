d9c46e19a9addc9be6f9377ac12097ba433951db
Ulf Bengtsson <Ulf.Bengtsson@Lexicon.se>
2022-04-11 12:14:29 +0200

    moved the Cars data handeling over to a service class - seperation of consern

--- WebAppCarsLobby/Controllers/CarsController.cs
+++ WebAppCarsLobby/Controllers/CarsController.cs
@@ -3,57 +3,53 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAppCarsLobby.Models;
 
 namespace WebAppCarsLobby.Controllers
 {
     public class CarsController : Controller
     {
-        static List<string[]> carsStorage;
+        CarService _carService;
 
         public CarsController()
         {
-            if (carsStorage == null)
-            {
-                carsStorage = new List<string[]>();
-                string[] saab = new string[] { "SAAB","900s","8500" };
-                string[] volvo = new string[] { "Volvo","740 GLT","4600" };
-                string[] bmw = new string[] { "BMW","M3","14900" };
-                carsStorage.Add(saab);
-                carsStorage.Add(volvo);
-                carsStorage.Add(bmw);
-            }
+            _carService = new CarService();
         }
 
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.Cars = carsStorage;
+            ViewBag.Cars = _carService.GetCars();
             return View();
         }
 
         [HttpGet]
         public IActionResult Create()
         {
-            ViewBag.Brands = new string[]
-            {
-                "SAAB",
-                "Volvo",
-                "BMW",
-                "Opel",
-                "VW",
-                "Mazda"
-            };
+            ViewBag.Brands = _carService.Getbrands();
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(string brand, string model, string price)
         {
-            string[] newCar = new string[] { brand, model, price };
-            
-            carsStorage.Add(newCar);
+            try
+            {
+                _carService.Create(brand, model, price);
 
-            return RedirectToAction("Index");
+                return RedirectToAction("Index");
+            }
+            catch (ArgumentException exceptionData)
+            {
+                ViewBag.ExceptionMsg = exceptionData.Message;
+            }
+
+            ViewBag.Brand = brand;
+            ViewBag.Model = model;
+            ViewBag.Price = price;
+            ViewBag.Brands = _carService.Getbrands();
+
+            return View();
         }
     }
 }
---
+++ WebAppCarsLobby/Models/CarService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppCarsLobby.Models
+{
+    public class CarService
+    {
+
+        static List<string[]> carsStorage;
+        static List<string> brandsStorage;
+
+        public CarService()
+        {
+            if (carsStorage == null)
+            {
+                carsStorage = new List<string[]>();
+                string[] saab = new string[] { "SAAB", "900s", "8500" };
+                string[] volvo = new string[] { "Volvo", "740 GLT", "4600" };
+                string[] bmw = new string[] { "BMW", "M3", "14900" };
+                carsStorage.Add(saab);
+                carsStorage.Add(volvo);
+                carsStorage.Add(bmw);
+
+                brandsStorage = new List<string>();
+                brandsStorage.Add("SAAB");
+                brandsStorage.Add("Volvo");
+                brandsStorage.Add("BMW");
+                brandsStorage.Add("Opel");
+                brandsStorage.Add("VW");
+                brandsStorage.Add("Mazda");
+
+            }
+        }
+
+        public List<string[]> GetCars()
+        {
+            return carsStorage;
+        }
+
+        public List<string> Getbrands()
+        {
+            return brandsStorage;
+        }
+
+        public void Create(string brand, string model, string price)
+        {
+            if (string.IsNullOrEmpty(brand))
+            {
+                throw new ArgumentException($"'{nameof(brand)}' cannot be null or empty.", nameof(brand));
+            }
+
+            if (string.IsNullOrEmpty(model))
+            {
+                throw new ArgumentException($"'{nameof(model)}' cannot be null or empty.", nameof(model));
+            }
+
+            if (string.IsNullOrEmpty(price))
+            {
+                throw new ArgumentException($"'{nameof(price)}' cannot be null or empty.", nameof(price));
+            }
+
+            if ( ! brandsStorage.Contains(brand))
+            {
+                brandsStorage.Add(brand);
+            }
+
+            carsStorage.Add(new string[] { brand, model, price });
+        }
+    }
+}
